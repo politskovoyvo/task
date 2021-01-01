2f219e9065c2da4f989465b50b643d0f52084b31
vladimir <polickovoy.v@wildberries.ru>
2021-01-01 21:58:51 +0300

    auth

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../../environments/environment';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, filter, flatMap, map, switchMap, tap, timeout } from 'rxjs/operators';
 import { jwtDecode } from 'jwt-decode';
 import { UntilDestroy } from '@ngneat/until-destroy';
-import { AuthCoreService } from './core/auth-core.service';
-import { User } from './models/user';
-import { TokenInfo } from './models/token-info';
+import { AuthCoreService } from './auth-core.service';
+import { User } from '../models/user';
+import { TokenInfo } from '../models/token-info';
+import { SESSION_STORAGE_KEYS } from '../models/session-storage-key';
+import { CookieService } from 'ngx-cookie-service';
 
 @UntilDestroy()
 @Injectable()
@@ -31,37 +33,25 @@ export class AuthService {
     private _authCoreService: AuthCoreService,
     private _activatedRoute: ActivatedRoute,
     private _router: Router,
-    private _http: HttpClient
+    private _http: HttpClient,
+    private _cookieService: CookieService
   ) {}
 
   public auth(login: string, password: string) {
-    return this._authCoreService.login
-      .post(
-        `${environment.urls.identityServerUrl}/connect/token`,
-        `grant_type=password&username=${login}&password=${password}&scope=offline_access api1`,
-        {
-          headers: this.getHeader(loginPosition),
-        }
-      )
+    
+    return this._authCoreService.login(login, password)
       .pipe(
         tap((tokenInfo: TokenInfo) => {
           this.updateLocalToken(tokenInfo);
-          localStorage.setItem('loginPosition', loginPosition.toString());
         })
       );
   }
 
   public refreshToken(): Observable<TokenInfo> {
     const refreshToken = localStorage.getItem(SESSION_STORAGE_KEYS.refreshToken);
-    return this._http
-      .post<TokenInfo>(
-        `${environment.urls.identityServerUrl}/connect/token`,
-        `refresh_token=${refreshToken}&grant_type=refresh_token`,
-        {
-          headers: this.getHeader(+localStorage.getItem('loginPosition')),
-        }
-      )
+    return this._authCoreService.refresh(refreshToken)
       .pipe(
+        catchError(err => this.redirectIfNeed()),
         tap((tokenInfo: TokenInfo) => {
           this.updateLocalToken(tokenInfo);
           if (this._router.url?.slice(0, 6) === '/login') {
@@ -69,12 +59,6 @@ export class AuthService {
           }
         })
       );
-  }
-
-  // /STOP/ ... New AUTH ... /STOP/
-
-  public removeCookie() {
-    return this._http.get(`${environment.urls.identityServerUrl}/profile/logout`, { withCredentials: true });
   }
 
   public logout(currentUrl?: string): Observable<boolean> {
@@ -98,32 +82,6 @@ export class AuthService {
       this._router.navigate(['/', 'login']);
       return of();
     }
-  }
-
-  public getTokenRequest(tokenData, isCourier?: boolean) {
-    // Если телефон с кеша пропал, проверяем запрос
-    if (tokenData.includes('refresh_token=undefined')) {
-      isCourier = true;
-    }
-
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: isCourier
-        ? 'Basic cGFzc3BvcnRfYXV0aDo2bkFaOVVyNnJweDI4RW0='
-        : 'Basic b3JyX2FkbWluOko5M0UxYmxEOVRpbw==',
-    });
-    return this._http
-      .post(
-        `${environment.urls.identityServerUrl}/connect/token`,
-        isCourier
-          ? `grant_type=password&username=passport&password=2UfEQ59o0S4BTyQ&scope=api_permission%20openid%20userInfo%20api1`
-          : `${tokenData}&scope=${encodeURIComponent(environment.tokenScopes)}`,
-        {
-          withCredentials: true,
-          headers,
-        }
-      )
-      .pipe(timeout(2000));
   }
 
   private redirectIfNeed() {
@@ -161,22 +119,11 @@ export class AuthService {
     const accessToken = tokenInfo.access_token;
     const refreshToken = tokenInfo.refresh_token;
     const decodeTokenInfo = this.getDecodedAccessToken(accessToken);
-    const permissions = {
-      regions: [],
-      position_id: +decodeTokenInfo.position_id,
-      do_permissions: decodeTokenInfo.permissions?.split(','),
-    };
-    this.user.access_token = accessToken;
-    this.user.refresh_token = refreshToken;
-    this.user.employee_id = +decodeTokenInfo.id;
-    this.user.family_name = decodeTokenInfo.name;
-    this.user.position_name = decodeTokenInfo.position_name;
-    this.user.isSuperUser = !!decodeTokenInfo.su;
+    this.user.accessToken = accessToken;
+    this.user.refreshToken = refreshToken;
 
-    this._epSubject.next(new EPermissions(permissions));
 
     localStorage.setItem(SESSION_STORAGE_KEYS.refreshToken, refreshToken);
-    localStorage.setItem(SESSION_STORAGE_KEYS.employee_id, this.user.employee_id?.toString());
     this._cookieService.set(
       'Authorization',
       `Bearer ${tokenInfo.access_token}`,
