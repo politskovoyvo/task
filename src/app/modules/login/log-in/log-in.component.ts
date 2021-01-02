3f431e4a1006bcccbedaa8f82271653c98a43917
vladimir <polickovoy.v@wildberries.ru>
2021-01-02 04:18:14 +0300

    edit global styles

--- src/app/modules/login/log-in/log-in.component.ts
+++ src/app/modules/login/log-in/log-in.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['../login.component.scss']
+  styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.router.navigate(['']);
+  }
 }
--- src/app/modules/login/register/register.component.ts
+++ src/app/modules/login/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['../login.component.scss']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
